3b63ddd793b85db3807b740df936de097433e8bd
psifio <mkrous@gmail.com>
2015-12-10 22:51:39 +0200

    Update runAll.R

--- runAll.R
+++ runAll.R
@@ -2,4 +2,7 @@
 source("https://raw.githubusercontent.com/psifio/gkouvas/master/graphics/ggplotemoji.R",
        echo=T,print.eval=T)
        
+#install.packages("gtrendsR")
+source("https://raw.githubusercontent.com/psifio/gkouvas/master/google/gTrends.R",
+       echo=T,print.eval=T)
        
